525ab84a913e2fa3ca48f1afd17e1e3fa8657a73
Ryan Naugle <rnaugle77@gmail.com>
2015-03-23 18:44:35 -0500

    translate class

---
+++ Project5/Translate/Translate.java
@@ -0,0 +1,10 @@
+package Translate;
+
+public class Translate{
+	public Mips.MipsFrame mframe;
+	java.util.LinkedList<Translate.Frag> frags;
+
+	public Translate(Mips.MipsFrame mf)
+	{ mframe = mf; }
+}
+
