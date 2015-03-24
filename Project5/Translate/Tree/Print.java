6f672c427f66058a63ab566d914d944305086a83
Ryan Naugle <rnaugle77@gmail.com>
2015-03-23 19:36:56 -0500

    printing tree

--- Project5/Translate/Tree/Print.java
+++ Project5/Translate/Tree/Print.java
@@ -1,83 +1,144 @@
 package Translate.Tree;
-
 public class Print implements IntVisitor{
+    private java.io.PrintStream out;
+    private int indent = 0;
+    private Stm s;
 
-	@Override
-	public void visit(BINOP n, int s) {
-		// TODO Auto-generated method stub
-		
-	}
+    public Print(){this.(System.out);}
 
-	@Override
-	public void visit(CALL n, int s) {
-		// TODO Auto-generated method stub
-		
-	}
+    public Print(java.io.PrintStream out, Stm s){
+	this.out = out;
+	this.s = s;
+    }
 
-	@Override
-	public void visit(CJUMP n, int s) {
-		// TODO Auto-generated method stub
-		
-	}
+    private void printIndent(){
+	for(int i=0; i<indent/2; i++)
+	    out.print(" ");
+    }
 
-	@Override
-	public void visit(CONST n, int s) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void visit(ESEQ n, int s) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void visit(EXP n, int s) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void visit(JUMP n, int s) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void visit(LABEL n, int s) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void visit(MEM n, int s) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void visit(MOVE n, int s) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void visit(NAME n, int s) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void visit(SEQ n, int s) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void visit(TEMP n, int s) {
-		// TODO Auto-generated method stub
-		
-	}
-
+@Override
+public void visit(BINOP n) {
+// TODO Auto-generated method stub
+    out.println("BINOP("+n.binop);
+    indent +=2; printIndent();
+    n.left.accept(this);
+    out.println();
+    n.right.accept(this);
+    indent -= 2;
+    out.println(")");
+}
+@Override
+public void visit(CALL n) {
+// TODO Auto-generated method stub
+    out.println("CALL(");
+    indent += 2; printIndent();
+    n.func.accept(this);
+    out.println();
+    for(int i=0; i<n.args.size(); i++){
+	n.args.get(i).accept(this);
+	out.println();
+    }
+    indent -= 2;
+    out.println(")");
+}
+@Override
+public void visit(CJUMP n) {
+// TODO Auto-generated method stub
+    out.println("CJUMP("+n.relop);
+    indent += 2; printIndent();
+    n.left.accept(this);
+    out.println();
+    n.right.accept(this);
+    out.println();
+    n.iftrue.toString();
+    out.println();
+    n.iffalse.toString();
+    indent =-2;
+    out.println(")");
+}
+@Override
+public void visit(CONST n) {
+// TODO Auto-generated method stub
+    out.print("CONST("+n.value+")");
+}
+@Override
+public void visit(ESEQ n) {
+// TODO Auto-generated method stub
+    out.println("ESEQ(");
+    indent += 2; printIndent();
+    n.stm.accept(this);
+    out.println();
+    n.exp.accept(this);
+    indent -= 2;
+    out.println(")");
+}
+@Override
+public void visit(EXP n) {
+// TODO Auto-generated method stub
+    out.println("EXP(");
+    indent += 2; printIndent();
+    n.exp.accept(this);
+    indent -= 2;
+    out.println(")");
+}
+@Override
+public void visit(JUMP n) {
+// TODO Auto-generated method stub
+    out.println("JUMP(");
+    indent += 2; printIndent();
+    n.exp.accept(this);
+    out.println();
+    for(int i=0; i<n.targets.size(); i++){
+	n.targets.get(i).toString();
+	out.println();
+    }
+    indent -= 2;
+    out.println(")");
+}
+@Override
+public void visit(LABEL n) {
+// TODO Auto-generated method stub
+    out.print("LABEL("+n.label.name+")");
+}
+@Override
+public void visit(MEM n) {
+// TODO Auto-generated method stub
+    out.println("MEM(");
+    indent += 2; printIndent();
+    n.exp.accept(this);
+    indent -= 2;
+    out.println(")");
+}
+@Override
+public void visit(MOVE n) {
+// TODO Auto-generated method stub
+ out.println("MOVE(");
+    indent += 2; printIndent();
+    n.dst.accept(this);
+    out.println();
+    n.src.accept(this);
+    indent -= 2;
+    out.println(")");
+}
+@Override
+public void visit(NAME n) {
+// TODO Auto-generated method stub
+    out.print("NAME"+n.label.name+")");
+}
+@Override
+public void visit(SEQ n, int s) {
+// TODO Auto-generated method stub
+    out.println("SEQ(");
+    indent += 2; printIndent();
+    n.left.accept(this);
+    out.println();
+    n.right.accept(this);
+    indent -= 2;
+    out.println(")");
+}
+@Override
+public void visit(TEMP n, int s) {
+// TODO Auto-generated method stub
+ out.print("TEMP("+n.temp.num+")");
+}
 }
